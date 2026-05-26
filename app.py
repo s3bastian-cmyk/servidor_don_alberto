c5141c09d6006d9dee672c695f3a6351dc7d4aa5
s3bastian-cmyk <murciamateussebastiancamilo@gmail.com>
2026-05-26 14:24:18 -0500

    guardado antes del merge

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+
 app = Flask(__name__)
-peritajes_db =[
+
+# Base de datos en memoria
+peritajes_db = [
     {"placa": "SCM-104", "estado": "cambio de aceite", "mecanico": "andres"},
-    {"placa": "kjg-405", "estado": "ajuste de cadena", "mecanico": "carlos"}
+    {"placa": "KJG-405", "estado": "ajuste de cadena", "mecanico": "carlos"}
 ]
+
 @app.route('/api/repuestos', methods=['GET'])
 def get_repuestos():
-        return jsonify({
-          "status": "online",
-	  "servidor": "Ubuntu de murcia:",
-          "hora_servidor": ["Bujias de Iridio","Filtro de aceite", "Aceite motul 7100"]
-        })
+    return jsonify({
+        "status": "online",
+        "servidor": "Ubuntu de murcia",
+        "repuestos_disponibles": ["Bujias de Iridio", "Filtro de aceite", "Aceite motul 7100"]
+    })
+
 @app.route('/api/registros', methods=['GET'])
 def get_registros():
     inventario = ["Bujias de Iridio", "Filtro de aceite", "Aceite motul 7100"]
-    hora_actual = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return jsonify({
          "inventario": inventario,
          "servidor": "Ubuntu de murcia",
          "hora_servidor": hora_actual
     })
-@app.route('/api/peritajes', methods=['GET','POST'])
+
+@app.route('/api/inventario', methods=['GET'])
+def inventario():
+    return jsonify({
+        "repuestos": [
+            "llantas",
+            "baterias",
+            "cascos"
+        ]
+    })
+
+@app.route('/api/peritajes', methods=['GET', 'POST'])
 def gestionar_peritajes():
     if request.method == 'POST':
          datos = request.get_json()
          if not datos or 'placa' not in datos:
              return jsonify({"error": "Falta la placa"}), 400
+         
          nuevo_peritaje = {
              "placa": datos.get('placa'),
              "estado": datos.get("estado", "ingresado"),
@@ -36,5 +53,7 @@ def gestionar_peritajes():
          return jsonify({"mensaje": "moto registrada con exito", "peritaje": nuevo_peritaje}), 201
     else:
          return jsonify(peritajes_db)
+
+# El bloque de arranque SIEMPRE al final absoluto del archivo
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
